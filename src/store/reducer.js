6cc936409268df447dc18a43f9074b28e29a7fa0
galubezzz <galubezzz@gmail.com>
2019-03-27 14:58:48 -0700

    add some fixes to reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,24 +1,32 @@
 const initialState = {
     result: null,
     calc_string: "",
-    value1: 0,
+    value1: "",
     operator: null,
 };
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case 'OPERATOR':
-            return {...state, operator: action.operator, value1: parseInt(state.calc_string), calc_string: ""};
+            if (state.operator){
+                return {...state, operator: action.operator};
+            } else {
+                return {...state, operator: action.operator, value1: parseInt(state.calc_string), calc_string: ""};
+            }
         case 'CALC':
-            return {...state, result: calculateResult(state.value1, parseInt(state.calc_string), state.operator)};
+            return {...state, result: calculateResult(state.value1, parseInt(state.calc_string), state.operator), calc_string: "", value1: "", operator: null};
         case 'ENTER_NUMBER':
             if (state.result){
-                return {...state, calc_string: action.number, result: null}
+                return {...state, calc_string: action.number.toString(), result: null}
             } else {
                 return {...state, calc_string: state.calc_string + action.number};
             }
         case 'DELETE_NUMBER':
-            return {...state, calc_string: deleteSymbol(state.calc_string)};
+            if (!state.result) {
+                return {...state, calc_string: deleteSymbol(state.calc_string)};
+            } else {
+                return {...state, result: null};
+            }
         default:
             return state;
     }
